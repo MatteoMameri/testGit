4e53c6c47f2a561d77aa2dfa021f6ade66d8aa0e
Matteo <etu51122@henallux.be>
2024-11-22 10:33:26 +0100

    commit

--- testGitApp/App.js
+++ testGitApp/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text } from 'react-native';
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import Settings from './screens/Settings';
+import Icon from 'react-native-vector-icons/Ionicons'; // Importer des icônes
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home" // Page de départ
+        screenOptions={({ route }) => ({
+          headerShown: false, // Enlever l'entête par défaut
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            // Assigner une icône en fonction de la route
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // Retourne une icône Ionicons
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue', // Couleur de l'icône active
+          tabBarInactiveTintColor: 'gray', // Couleur de l'icône inactive
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
