5f7cd1e7b4716665e12281e42a9b4fd26e06d984
Matteo <etu51122@henallux.be>
2024-11-22 14:03:00 +0100

    r

--- testGitApp/screens/Home.js
+++ testGitApp/screens/Home.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Home PageGGG</Text>
+      <Text>Home PageggggGGG</Text>
     </View>
   );
 }
--- testGitApp/screens/Profile.js
+++ testGitApp/screens/Profile.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text>Profile Page</Text>
+      <Text>llProfile gggPage</Text>
     </View>
   );
 }
